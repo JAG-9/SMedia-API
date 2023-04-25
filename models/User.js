95cf44722ba2df61aa15c1b6659114f4d71191a1
Jose Garcia <jose@Joses-MacBook-Air.local>
2023-04-25 17:51:40 -0400

    work on models

--- models/User.js
+++ models/User.js
@@ -1,46 +1,50 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const userSchema = new Schema(
     {
         username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
-        },
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+          email: {
+        type: String,
+        required: true,
+        unique: true,
+
+    
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please enter a correct email address",
+        ],
+           },
+
         thoughts: [
             {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought'
-            }
-        ],
-        friends: [
+              type: Schema.Types.ObjectId,
+              ref: "Thought",
+            },
+          ],
+
+          friends: [
             {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-
-            }
-        ]
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
+              type: Schema.Types.ObjectId,
+              ref: "User",
+            },
+          ],
         },
-            id: false
-    }
-);
+        {
+          toJSON: {
+            virtuals: true,
+          },
+          id: false,
+        }
+      );
 
-userSchema.virtual("friendCount").get(function () {
-    return this.friends.length;
-});
+      userSchema.virtual("friendCount").get(function () {
+        return this.friends.length;
+    });
 
-const User = model('User', userSchema);
-
-module.exports = User;
+    const User = model('User', userSchema);
+    module.exports = User;
